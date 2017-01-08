c29e1307e750e77a3fd7f7a46865ce2c1483d332
Dennis Lieu <dlieu@lieudennis.com>
2017-01-08 22:22:34 +0100

    Add bomb script and restructure files

---
+++ Source/Bomberman/Bomb.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Bomberman.h"
+#include "Bomb.h"
+
+// Sets default values
+ABomb::ABomb()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	TimeToExplode = 3.f;
+	CurrentTimeToExplode = TimeToExplode;
+
+	BombMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BombMesh"));
+	RootComponent = BombMesh;
+}
+
+// Called when the game starts or when spawned
+void ABomb::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABomb::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	CurrentTimeToExplode = FMath::Clamp<float>(CurrentTimeToExplode - DeltaSeconds, 0, TimeToExplode);
+	if (CurrentTimeToExplode <= 0)
+	{
+		Explode();
+	}
+}
+
+void ABomb::DidExplode_Implementation()
+{
+	FString BombDebugString = GetName();
+	UE_LOG(LogClass, Log, TEXT("did explode %s"), *BombDebugString);
+}
+
+void ABomb::Explode()
+{
+	FString BombDebugString = GetName();
+	FString DestroySuccess = (Destroy() ? "success" : "failed");
+
+	UE_LOG(LogClass, Log, TEXT("%s will explode: %s"), *BombDebugString, DestroySuccess);
+}
---
+++ Source/Bomberman/Bomb.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Bomb.generated.h"
+
+UCLASS()
+class BOMBERMAN_API ABomb : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABomb();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	FORCEINLINE UStaticMeshComponent* GetMesh() { return BombMesh; }
+
+	UFUNCTION(BlueprintNativeEvent)
+	void DidExplode();
+	void DidExplode_Implementation();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, VisibleAnywhere, Category = "Bomb", meta = (BlueprintProtected = "true"))
+	float TimeToExplode;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Bomb", meta = (BlueprintProtected = "true"))
+	UStaticMeshComponent* BombMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Bomb")
+	float CurrentTimeToExplode;
+
+	void Explode();
+};
